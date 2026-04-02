85e3f5e287ce40b030fda85d6988e63ea6a02150
Li-Bertygi <tokhwoo2000@gmail.com>
2026-04-02 23:22:11 +0900

    Init spring boot project

---
+++ src/main/kotlin/com/hyunwoo/jobselectiontracker/JobSelectionTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.hyunwoo.jobselectiontracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobSelectionTrackerApplication
+
+fun main(args: Array<String>) {
+    runApplication<JobSelectionTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyunwoo/jobselectiontracker/JobSelectionTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyunwoo.jobselectiontracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobSelectionTrackerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
